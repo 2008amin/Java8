9bc6d94d1fad300c947b9f17990c0d2a4f2dcf6f
A.andalibi <A.andalibi@Tosan.com>
2022-10-03 08:51:49 +0330

    init

--- src/main/java/com/amin/java8/Java8Application.java
+++ src/main/java/com/amin/java8/Java8Application.java
@@ -3,6 +3,7 @@ package com.amin.java8;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.math.BigDecimal;
 import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.function.Function;
@@ -13,11 +14,34 @@ import java.util.stream.Collectors;
 public class Java8Application {
 
 	private  static final ConcurrentHashMap<String, String> students = new ConcurrentHashMap();
-	private  static final ArrayList<Integer> numbers = new ArrayList<>();
+	private  static final ArrayList<Integer> numbers = new ArrayList<>(Arrays.asList(10,20,30));
+	private  static final ArrayList<Integer> numbers1 = new ArrayList<>(Arrays.asList(10,20,30));
+	private  static final ArrayList<Integer> numbers2 = new ArrayList<>(Arrays.asList(10,20));
+	private  static final ConcurrentHashMap<String, Object> studentss = new ConcurrentHashMap();
 
 
 	public static void main(String[] args) {
     	SpringApplication.run(Java8Application.class, args);
+
+    	Student student=new Student();
+    	student.setName("a");
+
+		Student student2=new Student();
+		student2.setName("b");
+
+
+		studentss.put("a",student);
+		studentss.put("a",student2);
+
+		removeDuplicateBetweenTwoCollections();
+
+    	/*switch (student.getName()) {
+			case "test":
+				break;
+			case "test1":
+				break;
+
+		}
 
 		testFunctionAndPredicate();
 
@@ -25,17 +49,15 @@ public class Java8Application {
 
 		testOptional();
 
-        getFamilies();
+        getFamilies();*/
 	}
 
 	private static void testFunctionAndPredicate() {
 
-		numbers.add(10);
-		numbers.add(20);
-		numbers.add(30);
 
 		numbers.stream()
-				.filter(i->i>11).map(x->x/10)
+				.filter(i->i>11)
+				.map(x->x/10)
 				.sorted(Comparator.reverseOrder())
 				.collect(Collectors.toList());
 
@@ -43,11 +65,16 @@ public class Java8Application {
 
 		Predicate<Integer> predicateOne= i->i>1;
 		Function<Integer,Integer> functionOne= I->I/10;
-		numbers.stream().filter(predicateOne).map(functionOne).sorted().collect(Collectors.toList());
+		numbers.stream()
+				.filter(predicateOne)
+				.map(functionOne)
+				.sorted()
+				.collect(Collectors.toList());
 	}
 
 	public static List<Student> getFamilies() {
-		return (List)students.keySet().stream().map((a) -> {
+		return (List)students.keySet()
+			.stream().map((a) -> {
 			Student student = new Student();
 			student.setFamily(a);
 			student.setName(a);
@@ -68,7 +95,9 @@ public class Java8Application {
 		students.put("a","aa");
 		students.put("b","bb");
 
-		students.keySet().stream().filter(x->x.equals("a")).collect(Collectors.toList());
+		students.keySet().stream()
+				.filter(x->x.equals("a"))
+				.collect(Collectors.toList());
 
 		students.keySet().stream().sorted().collect(Collectors.toList());
 
@@ -94,6 +123,18 @@ public class Java8Application {
 
 		//Optional.get - gets the value, value should be present
 		Integer value12 = b.get();
+	}
+
+
+	public static  void removeDuplicateBetweenTwoCollections()
+	{
+
+		List<Integer> duplicateItems = numbers1.stream()
+				.filter(two -> numbers2.stream()
+				.anyMatch(one -> one.toString().equals(two.toString())))
+				.collect(Collectors.toList());
+
+		numbers1.removeAll(duplicateItems);
 	}
 
 }
